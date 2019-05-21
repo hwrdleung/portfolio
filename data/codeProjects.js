9f197552e677e469d81523a0b7a8eda9af2fa061
Noodles01 <hwrdleung@gmail.com>
2019-05-21 13:37:40 -1000

    Modified resume, modified 'my work section'

--- data/codeProjects.js
+++ data/codeProjects.js
@@ -1,37 +1,8 @@
-export default [
-    {
-        name: 'Twitter Clone',
-        github: 'https://www.github.com/hwrdleung/twitter-clone',
-        projectLink: 'https://twitter-clone-mern.herokuapp.com',
-        pages: [
-            {
-                imageUrl: 'assets/project_images/twitterClone01.jpg',
-                text: `Twitter Clone is a full-stack social web application based on the functionality of Twitter.  Users can post their own "tweets", view their friends' tweets, "like" and reply to tweets, as well as "follow" other users.`
-            },
-            {
-                imageUrl: 'assets/project_images/twitterClone02.jpg',
-                text: `Once registered, users will be brought to their dashboard, where they will be able to post new tweets, change their photos, accept "follow requests" from other users, and view a feed of their friends' recent tweets.`
-            },
-            {
-                imageUrl: 'assets/project_images/twitterClone03.jpg',
-                text: `Users have full control over their account.  On the "settings" page, users can modify their personal information, change their privacy settings, set a new password, or delete their account if they wish to.  Enabling the "private tweets" setting ensures that only their followers are able to view their tweets.`
-            },
-            {
-                imageUrl: 'assets/project_images/twitterClone04.jpg',
-                text: `Twitter Clone's front-end was built in React and utilizes modular and reusable components.  It uses Redux as its state management system and Bootstrap for styling.  The back-end consists of a NodeJS server, a MongoDB database for storing user data, and Firebase Storage for storing images.`
-            },
-            {
-                imageUrl: 'assets/project_images/twitterClone05.jpg',
-                text: `Twitter Clone is a personal project that I created to learn the basics of the React framework and ecosystem.  From working on this project, I gained a better understanding of modular code, the Redux pattern, and the capabilities of Bootstrap.`
-            }
-        ]
-    },
-    {
+export default [{
         name: 'Creative Studio',
         github: 'https://www.github.com/hwrdleung/deckbuilder2',
         projectLink: 'https://deckbuilder2.herokuapp.com/',
-        pages: [
-            {
+        pages: [{
                 imageUrl: 'assets/project_images/creativeStudio01.jpg',
                 text: `Creative Studio is a presentation-builder single-page-application that brings Google Fonts library, Pixabay image search, image editing, and the browser's CSS styling capabilities conveniently into one place.  Presentations can be saved online for later access, or exported to PNG and PSD formats.`
             },
@@ -58,15 +29,39 @@ export default [
                 imageUrl: 'assets/project_images/creativeStudio05.jpg',
                 text: `Creative Studio is a personal project that I created to gain a better understanding of object-oriented programming, MVC architecture, and modular code.  Working on this project has also taught me about state management, asynchronous Javascript, and the importance of planning before writing any code.`
             }
-
+        ]
+    },
+    {
+        name: 'Twitter Clone',
+        github: 'https://www.github.com/hwrdleung/twitter-clone',
+        projectLink: 'https://twitter-clone-mern.herokuapp.com',
+        pages: [{
+                imageUrl: 'assets/project_images/twitterClone01.jpg',
+                text: `Twitter Clone is a full-stack social web application based on the functionality of Twitter.  Users can post their own "tweets", view their friends' tweets, "like" and reply to tweets, as well as "follow" other users.`
+            },
+            {
+                imageUrl: 'assets/project_images/twitterClone02.jpg',
+                text: `Once registered, users will be brought to their dashboard, where they will be able to post new tweets, change their photos, accept "follow requests" from other users, and view a feed of their friends' recent tweets.`
+            },
+            {
+                imageUrl: 'assets/project_images/twitterClone03.jpg',
+                text: `Users have full control over their account.  On the "settings" page, users can modify their personal information, change their privacy settings, set a new password, or delete their account if they wish to.  Enabling the "private tweets" setting ensures that only their followers are able to view their tweets.`
+            },
+            {
+                imageUrl: 'assets/project_images/twitterClone04.jpg',
+                text: `Twitter Clone's front-end was built in React and utilizes modular and reusable components.  It uses Redux as its state management system and Bootstrap for styling.  The back-end consists of a NodeJS server, a MongoDB database for storing user data, and Firebase Storage for storing images.`
+            },
+            {
+                imageUrl: 'assets/project_images/twitterClone05.jpg',
+                text: `Twitter Clone is a personal project that I created to learn the basics of the React framework and ecosystem.  From working on this project, I gained a better understanding of modular code, the Redux pattern, and the capabilities of Bootstrap.`
+            }
         ]
     },
     {
         name: 'GameTrader',
         github: 'https://www.github.com/hwrdleung/gametrade',
         projectLink: 'https://gametrader.herokuapp.com/',
-        pages: [
-            {
+        pages: [{
                 imageUrl: 'assets/project_images/gametrader01.jpg',
                 text: `GameTrader is a full-stack CRUD application that manages a mock video game trading club.  Its users can publicly list their video games and initiate "trade requests" with other users.  GameTrader helps its users keep track of their trade activity and connect with other gamers.`
             },
@@ -91,8 +86,7 @@ export default [
     {
         name: 'spfBoost.com',
         projectLink: 'https://www.spfboost.com/',
-        pages: [
-            {
+        pages: [{
                 imageUrl: 'assets/project_images/spf01.jpg',
                 text: `spfboost.com is a Squarespace website that <a href="https://www.jesslikes.work/">Jess</a>, graphic designer, created for her client.  For this project, Jess provided me with PSD mockups of her design, and I was responsible for implementing it by modifying the website's "custom CSS".`
             },
